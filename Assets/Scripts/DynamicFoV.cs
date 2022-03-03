212e968fe956ff6120cb60e6ed29a1b4bad12d81
Tom Lancaster <43844772+TinkerTavern@users.noreply.github.com>
2022-03-03 12:04:31 +0000

    FOV Changes without altering dependency

--- Assets/Scripts/DynamicFoV.cs
+++ Assets/Scripts/DynamicFoV.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
+using Sigtrap.VrTunnellingPro;
 
 public class DynamicFoV : MonoBehaviour
 {
     public Text screenText;
+    private TunnellingMobile tunnellingMobile;
     public static float fovScale = 1.0f;
 
     void Start()
     {
-        Sigtrap.VrTunnellingPro.TunnellingBase.effectCoverage = 1-fovScale;
+        tunnellingMobile = gameObject.transform.parent.gameObject.GetComponent<TunnellingMobile>();
+        tunnellingMobile.effectCoverage = 1-fovScale;
         screenText.text = string.Format("Scale: {0:F1}\n", fovScale);
     }
 }
